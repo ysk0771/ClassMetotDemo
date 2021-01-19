e75b1f579b94f5ad74f84cc8ce0df644c6ea48b3
Furkan Yasin <39018346+ysk0771@users.noreply.github.com>
2021-01-19 21:58:55 +0300

    gönder

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+       public int id;
+        
+       public string ad;
+        
+       public string soyad;
+
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        static string a = "Müsteri silindi!!!";
+        static string b = "Müşteri eklendi!!!";
+
+        public static void ekle(Musteri i, int id, string ad, string soyad)
+        {
+            i.id = id;
+            i.ad = ad;
+            i.soyad = soyad;
+
+            Console.WriteLine(b);
+        }
+        public static void sil(Musteri i)
+        {
+            i.id = -2;
+
+            i.ad = null;
+
+            i.soyad = null;
+
+            Console.WriteLine(a);
+
+        }
+
+        public static void listele(Musteri i, Musteri y , Musteri z , Musteri f )
+        {
+            Musteri[] Musteriler = new Musteri[] { i, y, z, f };
+
+            foreach (var musteri in Musteriler)
+            {
+                Console.WriteLine(i.id + " " + musteri.ad + " " + musteri.soyad);
+            }
+       
+        }
+
+        public static void listele(Musteri i)
+        {
+            Musteri[] Musteriler = new Musteri[] { i };
+
+            foreach (var musteri in Musteriler)
+            {
+                Console.WriteLine(i.id + " " + musteri.ad + " " + musteri.soyad);
+            }
+
+        }
+
+
+
+
+
+    }
+}
