797b34634fc7715ec70ad52f332b2b6e2c840f68
Furkan Yasin <39018346+ysk0771@users.noreply.github.com>
2021-01-23 02:03:37 +0300

    Yeni Odev  eklendi.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+
+    class MyDictionary<A>
+    {
+        A[] arrx;
+
+        public MyDictionary()
+        {
+           arrx = new A[0];
+        }
+
+        public void Add (A item)
+        {
+            
+            arrx = new A[arrx.Length];
+
+            A[] temp = arrx;
+
+            arrx = new A[arrx.Length + 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                arrx[i] = temp[i];
+            }
+
+            arrx[arrx.Length - 1] = item;
+
+            Console.WriteLine("Başarıyla eklendi,eleman sayınız  "+arrx.Length);
+        }
+
+       
+    
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string> { };
+
+            isimler.Add("Furkan");
+
+            isimler.Add("Yasin");
+
+            isimler.Add("Keskin");
+
+           
+        }
+    }
+}
